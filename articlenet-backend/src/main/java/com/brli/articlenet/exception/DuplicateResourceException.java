10fd075b8f00d02f51a3d3af981a936411fb1ada
brandon <ruipengyu.li@gmail.com>
2024-03-06 21:16:34 +0000

    updated to postgres

--- articlenet-backend/src/main/java/com/brli/articlenet/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.brli.articlenet.exception;
-
-public class DuplicateResourceException extends RuntimeException{
-    public DuplicateResourceException(String msg) {
-        super(msg);
-    }
-}
